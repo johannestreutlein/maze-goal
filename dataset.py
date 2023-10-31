07f36bb227a7c7ba8d0f46220fe5f0ff84184b0c
johannestreutlein <jonny.treutlein@gmail.com>
2023-10-31 13:09:09 -0700

    init

---
+++ dataset.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from maze_dataset.generation import LatticeMazeGenerators
+from maze_dataset.maze import LatticeMaze, TargetedLatticeMaze, SolvedMaze
+from maze_dataset.plotting import MazePlot
+
+from torch.utils import data
+
+# implementing my own maze dataset class,
+
+from torch.utils.data import Dataset
+from maze_dataset import MazeDataset, MazeDatasetConfig
+from maze_dataset.generation import LatticeMazeGenerators
+from maze_dataset.tokenization import MazeTokenizer, TokenizationMode
+
+
+def find_from_right(lst, item):
+  try:
+      index = lst[::-1].index(item)  # Reverse the list and find the item
+      return len(lst) - 1 - index  # Adjust the index for the original list
+  except ValueError:
+      return None  # Item not found
+
+
+class CustomMazeDataset(Dataset):
+    def __init__(self, grid_n=5, include_maze=False):
+        # Initialization code goes here
+        self.include_maze = include_maze
+        self.grid_n = grid_n
+        self.tokenizer = MazeTokenizer(max_grid_size=grid_n)
+        self.vocab_size = self.tokenizer.vocab_size
+
+        self.token_arr = self.tokenizer.token_arr
+
+        self.pad_token = self.tokenizer.padding_token_index
+
+        self.vocab_map = self.tokenizer.tokenizer_map.get
+        pass
+
+    def __getitem__(self, index):
+        # Replace this with your code to generate a sample given an index
+        # right now everything is random. I don't know how to create the same maze twice
+
+        start_pos = np.random.randint(self.grid_n,size=2)
+
+        maze = LatticeMazeGenerators.gen_dfs_percolation(grid_shape=np.array([self.grid_n, self.grid_n]),p=0.1)
+
+        goal_pos = np.random.choice([0,self.grid_n-1],2) # randomly choose goal
+
+
+        tgt_maze: TargetedLatticeMaze = TargetedLatticeMaze.from_lattice_maze(maze, start_pos, goal_pos)
+        solved_maze: SolvedMaze = SolvedMaze.from_targeted_lattice_maze(tgt_maze)
+
+        tokens = solved_maze.as_tokens(self.tokenizer)
+
+
+        ints = list(map(self.vocab_map, tokens))
+
+        goal_index = find_from_right(ints, 2)
+
+        ints_no_goal = ints[:goal_index] + ints[goal_index +3:]
+
+        start_index = goal_index
+        if self.include_maze:
+          return {'data': ints_no_goal, 'start_index': start_index, 'maze': solved_maze}
+        else:
+          return {'data': ints_no_goal, 'start_index': start_index}
+
+    def __len__(self):
+        # Replace this with your code to return the length of the dataset
+        return 1000000000
+
+
+def collate_fn(batch):
+    # Get the maximum sequence length in the batch
+    max_len = max(len(item['data']) for item in batch)
+
+    # Pad the sequences and store them in a list
+    padded_data = np.array([np.pad(item['data'], (0, max_len - len(item['data'])), 'constant') for item in batch])
+
+    # Store the end indices in a list
+    end_indices = np.array([len(item['data']) - 1 for item in batch])
+
+    # Initialize the collated batch with the padded data and end indices
+    collated_batch = {'data': padded_data, 'end_index': end_indices}
+
+    # Add any other keys from the batch elements
+    for key in batch[0]:
+        if key not in collated_batch:
+            collated_batch[key] = np.array([item[key] for item in batch])
+
+    return collated_batch
+
+
+
+
+
+#def numpy_collate(batch):
+#  # define collate here, with padding
+#
+#  return tree_map(np.asarray, data.default_collate(batch))
+
+class NumpyLoader(data.DataLoader):
+  def __init__(self, dataset, batch_size=1,
+                shuffle=False, sampler=None,
+                batch_sampler=None, num_workers=0,
+                pin_memory=False, drop_last=False,
+                timeout=0, worker_init_fn=None):
+    super(self.__class__, self).__init__(dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        sampler=sampler,
+        batch_sampler=batch_sampler,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+        drop_last=drop_last,
+        timeout=timeout,
+        worker_init_fn=worker_init_fn)
---
+++ main.py
@@ -0,0 +1,221 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import jax
+import flax
+from flax import linen as nn
+from jax import numpy as jnp
+from jax import random
+from einops import einsum, rearrange
+import optax
+import matplotlib.pyplot as plt
+import pickle
+from google.colab import drive
+
+import numpy as np
+
+import orbax.checkpoint
+from flax.training import orbax_utils
+
+
+import os
+import uuid
+import datetime
+
+from maze_dataset.plotting import MazePlot
+from maze_dataset.tokenization.token_utils import strings_to_coords
+
+from dataset import CustomMazeDataset
+from dataset import NumpyLoader
+
+from model import TransformerLM, TransformerConfig
+
+
+
+
+# config details
+checkpoint_path = None
+save = True
+
+np_seed = 0
+jnp_seed = 0
+
+batch_size = 128
+lr = 1e-4
+n_train_steps = 10000000
+
+save_every_n_steps = 100
+
+n_worker = 2
+
+#n_eval = 1024
+emb_dim: int = 256
+num_heads: int = 16
+num_layers: int = 12
+qkv_dim: int = 256#512
+mlp_dim: int = 1024#2048
+max_len = 256
+
+grid_n = 5
+
+
+
+def main():
+    @jax.jit
+    def train_step(state, batch):
+        params = state['params']
+        opt_state = state['opt_state']
+        loss, grads = jax.value_and_grad(loss_fn)(params, batch)
+        updates, opt_state = tx.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        step = state['step'] + 1
+
+        return {'params': params, 'opt_state': opt_state, 'loss': loss, 'step': step}
+
+    @jax.jit
+    def eval_step(state, batch):
+        params = state['params']
+        loss = loss_fn(params, batch)
+        return loss
+
+    dataset = CustomMazeDataset(include_maze=False)
+    train_loader = NumpyLoader(dataset, batch_size=batch_size, num_workers=n_worker)
+
+    losses = []
+    eval_losses = []
+
+    key = random.PRNGKey(jnp_seed)
+    rng, key = random.split(key)
+
+    config = TransformerConfig(
+        vocab_size=vocab_size,
+        output_vocab_size=vocab_size,
+        max_len=max_len,
+        emb_dim=emb_dim,
+        num_heads=num_heads,
+        num_layers=num_layers,
+        qkv_dim=qkv_dim,
+        mlp_dim=mlp_dim
+    )
+
+    model = TransformerLM(config=config)
+
+    def loss_fn(params, batch):
+        preds, act = model.apply(params, batch['data'])
+        preds = preds[:, 0:-1]
+        targets = batch['data'][:, 1:]
+        idx = jnp.arange(targets.shape[1])[None, :]
+        mask = jnp.where((idx <= batch['end_index'][:, None]) & (idx >= batch['start_index'][:, None]), 1., 0.)
+
+        loss = optax.softmax_cross_entropy_with_integer_labels(
+            logits=preds,
+            labels=targets
+        ) * mask
+
+        loss = loss.sum() / mask.sum()
+
+        return loss
+
+    tx = optax.adamw(lr)
+
+    x = next(iter(train_loader))
+    params = model.init(rng, x['data'])
+
+    apply_fn = jax.jit(model.apply)
+
+    opt_state = tx.init(params)
+
+    state = {'params': params, 'opt_state': opt_state, 'loss': 0., 'step': 0}
+
+    # checkpoint management / loading model
+
+    if save and not checkpoint_path:
+        # make new run dir ect
+
+        # Get the current date and time
+        current_datetime = datetime.datetime.now()
+
+        # Create a directory name with the date and unique ID
+        checkpoint_dir_name = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+
+        # Create the full path for the checkpoint directory
+        checkpoint_path = os.path.join(base_path, checkpoint_dir_name)
+
+        # Check if the directory already exists
+        if not os.path.exists(checkpoint_path):
+            # Create the directory
+            os.makedirs(checkpoint_path)
+            print(f"Checkpoint directory created: {checkpoint_path}")
+        else:
+            print(f"Checkpoint directory already exists: {checkpoint_path}")
+
+    if checkpoint_path:
+        orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
+        options = orbax.checkpoint.CheckpointManagerOptions(max_to_keep=100)
+        checkpoint_manager = orbax.checkpoint.CheckpointManager(checkpoint_path, orbax_checkpointer, options)
+
+        dummy_dict = {
+            'state': state}
+
+
+        step = checkpoint_manager.latest_step()
+
+        if step:
+            print(f'loading step {step}')
+            load_dict = checkpoint_manager.restore(step, items=dummy_dict)
+            state = load_dict['state']
+
+
+    # training loop
+
+    import time
+
+    # do the actual training
+    loop_time = time.time()
+    # start_time = time.time()
+    # end_time = time.time()
+
+    for n, batch in enumerate(train_loader):
+
+        if save and n % save_every_n_steps == 0:
+            save_step = state['step']
+            print(f'saving at step {save_step}')
+            save_dict = {'state': state,
+                         'loss': jnp.array(losses)
+                         }
+            save_args = orbax_utils.save_args_from_target(save_dict)
+            checkpoint_manager.save(save_step, save_dict, save_kwargs={'save_args': save_args})
+
+            plt.plot(losses)
+            plt.yscale('log')
+            plt.show()
+
+
+        if n >= n_train_steps:
+            break
+        # print('non train step stuff: {:.5f}'.format(loop_time-old_time-end_time+start_time))
+        # del batch['maze']
+
+        # with open(path.join(PATH,'train_state_step_{}.p'.format(state['opt_state'][-1].count)), 'wb') as fp:
+        #  pickle.dump(state, fp)
+
+        # start_time = time.time()
+        state = train_step(state, batch)
+        # [insert code for Part 1 here]
+        # end_time = time.time()
+        # print("Time for Train step: {:.5f} seconds".format(end_time - start_time))
+
+        losses.append(state['loss'])
+
+        old_time = loop_time
+        loop_time = time.time()
+        print('steps per second: {:.5f}'.format(save_every_n_steps / (loop_time - old_time)))
+        print(f'step: {state["step"]}')
+        print('loss: {}'.format(state['loss']))
+
+        # eval_loss = eval_step(state, eval_batch)
+        # print(f'eval_loss: {eval_loss}')
+        # eval_losses.append(eval_loss)
+
+
+if __name__ == '__main__':
+    main()
